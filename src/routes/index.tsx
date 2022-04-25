372b4f874b6b7ef2c3aad5bd1a45a658eddfbd53
Murilo Lemes <murilo.lemes.mhl@gmail.com>
2022-04-24 21:16:32 -0300

    Adicionando a rota 'calculator'

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,11 @@
 import { Routes as Router, Route } from 'react-router-dom';
 
 import { Home } from '../pages/Home'
+import { Calculator } from '../pages/Calculator';
 
 export const Routes: React.FC = () => (
   <Router>
     <Route path='/' element={<Home />} />
+    <Route path='/calculadora' element={<Calculator />} />
   </Router>
 )
