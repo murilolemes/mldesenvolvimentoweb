eebb95ac470dca35bf7c7d2450ad779a6f8d2754
Murilo Lemes <murilo.lemes.mhl@gmail.com>
2022-04-26 16:46:53 -0300

    Ajustes nas rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,9 +3,11 @@ import { Routes as Router, Route } from 'react-router-dom';
 import { Home } from '../pages/Home'
 import { Calculator } from '../pages/Calculator';
 
-export const Routes: React.FC = () => (
-  <Router>
-    <Route path='/' element={<Home />} />
-    <Route path='/calculadora' element={<Calculator />} />
-  </Router>
-)
+export function Routes() {
+  return (
+    <Router>
+      <Route path='/' element={<Home />} />
+      <Route path='/calculadora' element={<Calculator />} />
+    </Router>
+  )
+}
