6447b6f5ce3d07376574f440211ec8c9855911cd
Murilo Lemes <murilo.lemes.mhl@gmail.com>
2022-04-28 16:51:11 -0300

    Adicionando React Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
+import Modal from 'react-modal';
 
 import { Header } from './components/Header'
 import { Routes } from './routes'
@@ -6,6 +7,8 @@ import { Footer } from './components/Footer';
 import GlobalStyle from './styles/global';
 
 export function App() {
+  Modal.setAppElement('#root')
+
   return (
     <>
       <BrowserRouter>
