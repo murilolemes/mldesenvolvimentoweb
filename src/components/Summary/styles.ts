7a3daebb86f8fb0f3a7f54924495278cab58d3f0
Murilo Lemes <murilo.lemes.mhl@gmail.com>
2022-04-30 15:50:51 -0300

    Adicionando componente summary

---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+
+  div {
+    background: var(--primary-light);
+    padding: 1.5rem 2rem;
+    border-radius: 0.25rem;
+    color: var(--background);
+
+    header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      p {
+        font-size: 1.8rem;
+      }
+    }
+
+    strong {
+      display: block;
+      margin-top: 1rem;
+      font-size: 2.3rem;
+      font-weight: 500;
+      line-height: 3rem;
+      text-align: center;
+    }
+
+    &:last-child {
+      background: var(--green);
+      color: var(--primary-light);
+    }
+  }
+`;
