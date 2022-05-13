d8e07f91552a84d533be239de6c0ff205ef4668c
Murilo Lemes <murilo.lemes.mhl@gmail.com>
2022-05-13 17:01:17 -0300

    Adicionando calculo de valores dos cards

--- src/utils/getBalance.ts
+++ src/utils/getBalance.ts
@@ -1,5 +1,31 @@
-import
+interface transaction {
+  amount: number;
+  type: string;
+}
 
-export function getBalance(transactions: Transaction) {
-  const { income, outcome } = transactions.redul
-};
+export function getBalance(transactions: transaction[]) {
+  const balance = transactions.reduce((acc, transaction) => {
+    const transactionDeposit = transaction.amount;
+    const transactionWithdraw = transaction.amount;
+
+    switch (transaction.type) {
+      case 'deposit':
+        acc.deposit += transactionDeposit;
+        break;
+      case 'withdraw':
+        acc.withdraw += transactionWithdraw;
+        break;
+      default:
+        break;
+    }
+    return acc;
+  },
+    {
+      deposit: 0,
+      withdraw: 0,
+      total: 0
+    }
+  )
+  balance.total = balance.deposit - balance.withdraw;
+  return balance
+}
