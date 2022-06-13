5b4898eb9a70c992161965813882686d5f2c061d
Murilo Lemes <murilo.lemes.mhl@gmail.com>
2022-06-13 09:29:40 -0300

    Criando card de pokemon

---
+++ src/components/CardPokemon/index.tsx
@@ -0,0 +1,25 @@
+import { usePokemons } from '../../hooks/PokemonsContext';
+import { FiX } from "react-icons/fi";
+
+import { Container, Card } from "./styles";
+
+export function CardPokemon() {
+  const { pokemons } = usePokemons();
+  return (
+    <Container>
+      {pokemons.map(pokemon => (
+        <Card key={pokemon.id}>
+          <h1>{pokemon.name}</h1>
+          <img src={pokemon.img} alt={pokemon.name} />
+          <div className='descriptions'>
+            <p>Tipo:</p>
+            <p>{pokemon.type}</p>
+          </div>
+          <div className="btnRemove">
+            <button type='button' id='remove'><FiX /> Remover</button>
+          </div>
+        </Card>
+      ))}
+    </Container>
+  )
+}
---
+++ src/components/CardPokemon/styles.ts
@@ -0,0 +1,92 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const Card = styled.div`
+  width: 16rem;
+  height: 20rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid var(--secondary);
+  border-radius: 0.25rem;
+  box-shadow: 0 0 4px 1px var(--secondary-light);
+  transition: 0.2s;
+
+  &:hover {
+    background: var(--secondary);
+    border-color: var(--primary);
+    box-shadow: 0 0 4px 1px var(--primary-light);
+
+    .descriptions {
+      p {
+        color: var(--background);
+      }
+    }
+  }
+
+  img {
+    width: 10rem;
+    height: 10rem;
+  }
+
+  .descriptions {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0.5rem 1rem;
+    background-color: var(--secondary-light);
+
+    & + div {
+      margin-top: 1rem;
+    }
+
+    p:first-child {
+      font-size: 1.25rem;
+      font-weight: 500;
+      color: var(--background);
+    }
+  }
+
+  .btnRemove {
+    width: 100%;
+    padding: 0 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    button {
+      width: 100%;
+      height: 2rem;
+      border: none;
+      border-radius: 0.25rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: var(--primary-light);
+      font-weight: 500;
+      transition: filter 0.2s;
+
+      &:hover {
+        filter: brightness(0.9);
+      }
+
+      svg {
+        width: 1.125rem;
+        height: 1.125rem;
+        margin-right: 0.25rem;
+      }
+    }
+
+    #remove {
+      background: var(--red);
+    }
+
+    #btnAdd {
+      background: var(--green);
+    }
+  }
+`;
