4e76d18dcc3e2feb2469a1bef1170eebc23f9ed8
Murilo Lemes <murilo.lemes.mhl@gmail.com>
2022-07-11 21:34:27 -0300

    Estilizando card de pokemons

--- src/components/CardPokemon/index.tsx
+++ src/components/CardPokemon/index.tsx
@@ -6,24 +6,50 @@ import { Container, Card } from "./styles";
 export function CardPokemon() {
   const { pokemons, deletePokemon } = usePokemons();
 
-  pokemons.map((pokemon) => console.log('aqui', pokemon.stats))
+  pokemons.map((pokemon) => pokemon.stats.map((pk) => console.log('teste', pk)))
   // console.log('1', pokemons)
+
+  const colorBg = document.getElementById('colorBackground');
+
+  if (colorBg) {
+    pokemons.map((pokemon) => colorBg.style.background = pokemon.type.color);
+  }
 
   return (
     <Container>
       {pokemons.map(pokemon => (
-        <Card key={pokemon.id}>
-          <h1>{pokemon.name.replace(/-/g, ' ')}</h1>
-          <p>{pokemon.id}</p>
+        <Card key={pokemon.id} id='colorBackground'>
+          <div id="title">
+            <h1>{pokemon.name.replace(/-/g, ' ')}</h1>
+            <p>#{pokemon.id}</p>
+          </div>
           <div id="imgPokemon">
             <img src={pokemon.img} alt={pokemon.name} />
           </div>
           <div className='descriptions'>
             <div className="stats">
               <p>Stats</p>
-              {/* {pokemon.stats.map((teste) => (
-                <p>{teste}</p>
-              ))} */}
+              {/* {pokemon.stats.map((poke) => ( */}
+              <div>
+                <p>HP:<span>63</span></p>
+                <p>Attack:<span>60</span></p>
+                <p>Defense:<span>55</span></p>
+                <p>Special Attack:<span>50</span></p>
+                <p>Special Defense:<span>50</span></p>
+                <p>Speed:<span>71</span></p>
+              </div>
+              {/* ))} */}
+            </div>
+            <div className="skills">
+              <p>Skills</p>
+              <div>
+                <p>HP:<span>63</span></p>
+                <p>Attack:<span>60</span></p>
+                <p>Defense:<span>55</span></p>
+                <p>Special Attack:<span>50</span></p>
+                <p>Special Defense:<span>50</span></p>
+                <p>Speed:<span>71</span></p>
+              </div>
             </div>
           </div>
           <div className="btnRemove">
--- src/components/CardPokemon/styles.ts
+++ src/components/CardPokemon/styles.ts
@@ -3,8 +3,8 @@ import styled from "styled-components";
 export const Container = styled.div``;
 
 export const Card = styled.div`
-  width: 16rem;
-  height: 20rem;
+  width: 20rem;
+  height: 28rem;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -14,36 +14,86 @@ export const Card = styled.div`
   box-shadow: 0 0 4px 1px var(--primary);
   background: var(--secondary-light);
   transition: 0.2s;
-  padding: 1rem;
 
-  h1 {
-    text-align: center;
-    color: var(--white-100);
+  #title {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0.375rem 1rem 0;
+
+    h1 {
+      text-align: center;
+      color: var(--white-100);
+    }
+
+    p{
+      font-size: 0.75rem;
+      filter: opacity(0.4);
+    }
   }
 
-  img {
-    flex: 1;
+  #imgPokemon {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+
+    img {
+      width: 13rem;
+      height: 13rem;
+      position: absolute;
+      top: 0;
+    }
   }
 
   .descriptions {
     width: 100%;
+    height: 100%;
     display: flex;
-    flex-direction: row;
-    align-items: center;
+    align-items: flex-start;
     justify-content: space-between;
     padding: 0.5rem 1rem;
     background-color: var(--white-100);
-    border-radius: 0.25rem;
-    color: var(--blue-800);
 
-    & + div {
-      margin-top: 1rem;
+    .stats, .skills {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: flex-start;
+
+      p {
+        width: 100%;
+        text-align: center;
+        font-weight: 500;
+        color: var(--blue-900);
+      }
+
+      div {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0.25rem;
+
+        p {
+          width: 100%;
+          font-size: 0.75rem;
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        }
+      }
     }
 
-    p:first-child {
-      font-size: 1.25rem;
-      font-weight: 500;
-      color: var(--blue-900);
+    .skills {
+      border-left: 1px solid var(--blue-800);
     }
   }
 
@@ -62,7 +112,7 @@ export const Card = styled.div`
       width: 100%;
       height: 2rem;
       border: none;
-      border-radius: 0.25rem;
+      border-radius: 0 0 0.25rem 0.25rem;
       display: flex;
       align-items: center;
       justify-content: center;
