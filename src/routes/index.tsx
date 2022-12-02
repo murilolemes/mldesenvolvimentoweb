02aaa2c30868d75ccfcd39dde1f77a6f611157b9
Murilo Lemes <murilo.lemes.mhl@gmail.com>
2022-12-02 14:41:36 -0300

    Adicionando rota Shop

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import { Home } from '../pages/Home'
 import { Calculator } from '../pages/Calculator';
 import { Finances } from '../pages/Finances'
 import { Pokemon } from '../pages/Pokemon';
+import { Shop } from '../pages/Shop';
 
 export function Routes() {
   return (
@@ -12,6 +13,7 @@ export function Routes() {
       <Route path='/calculadora' element={<Calculator />} />
       <Route path='/financas' element={<Finances />} />
       <Route path='/pokemon' element={<Pokemon />} />
+      <Route path='/Shop' element={<Shop />} />
     </Router>
   )
 }
