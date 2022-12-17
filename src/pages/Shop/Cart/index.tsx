114cf84f2bbafcf5076d9c2173e7d477b5eee941
Murilo Lemes <murilo.lemes.mhl@gmail.com>
2022-12-17 11:45:02 -0300

    Alterações na página Cart

--- src/pages/Shop/Cart/index.tsx
+++ src/pages/Shop/Cart/index.tsx
@@ -1,30 +1,34 @@
 import { Link } from 'react-router-dom';
-import { TiArrowLeftThick } from 'react-icons/ti'
+import { TiArrowLeftThick } from 'react-icons/ti';
+
+import { MyCart } from '../../../components/ShopComponent/MyCart';
+import { ShopProvider } from '../../../hooks/ShopContext';
 
 import { Container, Content, MyShopHeader, BtnBuy, Total } from './styles';
-import { MyCart } from '../../../components/CardShop/MyCart';
+import { QntdItens, ValueTotal } from '../../../components/ShopComponent/ComponentsCart';
 
 export function Cart() {
   return (
-    <Container>
-      <Content>
-        <MyShopHeader>
-          <div className="title">
-            <h1>Meu carrinho</h1>
-            <Link to='/shop' type='button'><TiArrowLeftThick size={20} /> Voltar</Link>
-          </div>
-          <p>Total R$ <span>45,00</span></p>
-          <BtnBuy type='button'>Finalizar Compra</BtnBuy>
-        </MyShopHeader>
-        <MyCart />
-        <Total>
-          <div className="itens">
-            <p>Quantidade de itens selecionados: <span>10</span></p>
-            <p>Total R$ <span>45,00</span></p>
-          </div>
-          <BtnBuy type='button'>Finalizar Compra</BtnBuy>
-        </Total>
-      </Content>
-    </Container>
+    <ShopProvider>
+      <Container>
+        <Content>
+          <MyShopHeader>
+            <div className="title">
+              <h1>Meu carrinho</h1>
+              <Link to='/shop' type='button'><TiArrowLeftThick size={20} /> Voltar</Link>
+            </div>
+            <BtnBuy type='button'>Finalizar Compra</BtnBuy>
+          </MyShopHeader>
+          <MyCart />
+          <Total>
+            <div className="itens">
+              <p>Quantidade de itens selecionados: <QntdItens /></p>
+              <p>Total <ValueTotal /></p>
+            </div>
+            <BtnBuy type='button'>Finalizar Compra</BtnBuy>
+          </Total>
+        </Content>
+      </Container>
+    </ShopProvider>
   )
 }
