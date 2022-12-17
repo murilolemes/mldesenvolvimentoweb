05e307652d693d934c955d687b25bf20b19ccb66
Murilo Lemes <murilo.lemes.mhl@gmail.com>
2022-12-17 11:46:01 -0300

    Alterações na página Shop

--- src/pages/Shop/index.tsx
+++ src/pages/Shop/index.tsx
@@ -1,33 +1,25 @@
-import { FiShoppingBag } from 'react-icons/fi'
-import { Link } from 'react-router-dom';
-import { FaSearch } from 'react-icons/fa';
-
-
+import { ShopProvider } from '../../hooks/ShopContext';
+import { CardShop } from '../../components/ShopComponent/CardShop';
+import { ButtonCart } from '../../components/ShopComponent/ButtonCart';
 
 import { Container, Content, MyShopHeader } from './styles';
-import { CardShop } from '../../components/CardShop';
+import { DivSearchProduct } from '../../components/ShopComponent/ComponentsCart';
 
 export function Shop() {
   return (
-    <Container>
-      <Content>
-        <MyShopHeader>
-          <div id="title">
-            <h1>Minha loja</h1>
-            <Link to='/shop/carrinho' id="buttonCart">
-              <FiShoppingBag size={24} />
-              <span>2</span>
-            </Link>
-          </div>
-          <div id="inputSearch">
-            <input name="search" placeholder='Pesquisar' />
-            <button type='button'>
-              <FaSearch size={18} />
-            </button>
-          </div>
-        </MyShopHeader>
-        <CardShop />
-      </Content>
-    </Container>
+    <ShopProvider>
+      <Container>
+        <Content>
+          <MyShopHeader>
+            <div id="title">
+              <h1>Minha loja</h1>
+              <ButtonCart />
+            </div>
+            <DivSearchProduct />
+          </MyShopHeader>
+          <CardShop />
+        </Content>
+      </Container>
+    </ShopProvider>
   )
 }
