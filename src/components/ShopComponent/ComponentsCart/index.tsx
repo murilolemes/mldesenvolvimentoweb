03c6b8b06a3ccb23b72164b47246a75d168f2126
Murilo Lemes <murilo.lemes.mhl@gmail.com>
2022-12-19 15:39:37 -0300

    Removendo input de pesquisa

--- src/components/ShopComponent/ComponentsCart/index.tsx
+++ src/components/ShopComponent/ComponentsCart/index.tsx
@@ -1,10 +1,5 @@
-import { useState } from 'react';
-import { FaSearch } from 'react-icons/fa';
-
 import { useShop } from "../../../hooks/ShopContext"
 import { formatValue } from "../../../utils/formatValue";
-
-import { ContainerInputSearch } from './styles';
 
 export function ValueTotal() {
   const { cartProducts } = useShop();
@@ -22,26 +17,5 @@ export function QntdItens() {
   const { cartProducts } = useShop();
   return (
     <span>{cartProducts.length}</span>
-  )
-}
-
-export function DivSearchProduct() {
-  const { searchProduct } = useShop();
-  const [search, setSearch] = useState('')
-
-  return (
-    <ContainerInputSearch>
-      <form action=''>
-        <input
-          name="search"
-          placeholder='Pesquisar'
-          value={search}
-          onChange={e => setSearch(e.target.value)}
-        />
-        <button onClick={() => searchProduct(search)}>
-          <FaSearch size={18} />
-        </button>
-      </form>
-    </ContainerInputSearch>
   )
 }
