559538072109ea479ca51150bc8eff04ba5b3fab
Murilo Lemes <murilo.lemes.mhl@gmail.com>
2023-03-09 11:42:28 -0300

    Adicionando pagina de pokemons favoritos

---
+++ src/pages/Pokemon/FavoritePokemons/index.tsx
@@ -0,0 +1,14 @@
+import { FavoriteCards } from '../../../components/PokemonComponents/FavoriteCards';
+import { PokemonProvider } from '../../../hooks/PokemonsContext';
+
+import { Container } from "./styles";
+
+export function FavoritePokemons() {
+  return (
+    <PokemonProvider>
+      <Container>
+        <FavoriteCards />
+      </Container>
+    </PokemonProvider>
+  )
+}
---
+++ src/pages/Pokemon/FavoritePokemons/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  padding: 2rem 1rem 1rem;
+`;
+
